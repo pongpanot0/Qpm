740d18fa630826e443cf3f5d1e1b2382883a3996
Phongpanot Samarkarn <phongpanots@qpmgroup.local>
2020-11-14 17:13:18 +0700

    first commit

---
+++ src/componets/config.js
@@ -0,0 +1,13 @@
+import { Config } from "pretty-format/build/types";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBpb2jP0_ZIbgYZV2cXptjrpDYyfWQX7gk",
+    authDomain: "trainingproke.firebaseapp.com",
+    databaseURL: "https://trainingproke.firebaseio.com",
+    projectId: "trainingproke",
+    storageBucket: "trainingproke.appspot.com",
+    messagingSenderId: "620913178625",
+    appId: "1:620913178625:web:2afb05a9dc673efbe2e800",
+    measurementId: "G-F14XBXPWEZ"
+  };
+export default Config
